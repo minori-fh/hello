6838060a1e9ee031457b9e6d8375964ccf24092c
Minori Hashimoto <minori.fh@gmail.com>
2020-03-15 13:13:41 -0700

    add nav bar component to app.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,11 @@
 import React from "react";
+import "./nav.css";
+
+const Nav = () => (
+    <div className="nav">
+        <p>portfolio</p>
+        <p>about</p>
+    </div>
+);
 
 export default Nav;

edee52432fe570f79db547859977cbb0d850abb6
Minori Hashimoto <minori.fh@gmail.com>
2020-03-15 13:33:18 -0700

    add contact logo images and create sidebar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Nav from "./components/Nav/Nav";
+import SidebarContactMe from "./components/SidebarContactMe/SidebarContactMe";
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <SidebarContactMe/>
     </div>
   );
 }
--- src/components/SidebarContactMe/SidebarContactMe.js
+++ src/components/SidebarContactMe/SidebarContactMe.js
@@ -1,4 +1,17 @@
 import React from "react";
 
+import "./sidebar.css"
+
+import MailLogo from "./mail.png";
+import LinkedInLogo from "./linkedin.png";
+import GithubLogo from "./github.png";
+
+const SidebarContactMe = () => (
+    <div className="sidebar">
+        <img className="contactLogo" src={MailLogo}/>
+        <img className="contactLogo" src={LinkedInLogo}/>
+        <img className="contactLogo" src={GithubLogo}/>
+    </div>
+);
 
 export default SidebarContactMe; 
